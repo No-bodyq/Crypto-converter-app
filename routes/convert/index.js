8313f27b054ba43ff3a612c667dc741fc1b95797
Asher <theforce289@gmail.com>
2024-01-10 13:48:58 +0100

    feat: update post request for currency conversion

--- routes/convert/index.js
+++ routes/convert/index.js
@@ -2,10 +2,23 @@
 
 module.exports = async function (app, opts) {
   app.post("/", async function (request, reply) {
-    const data = await app.convert(
-      request.body["enteredCurrency"],
-      request.body["convertedCurrency"]
+    let enteredCurr = request.body["enteredCurrency"];
+    let convertedCurr = request.body["convertedCurrency"];
+    let data = await app.convert(enteredCurr, convertedCurr);
+    const currencies = await app.getCurrencies();
+    let oldCurr = currencies.find((currency) => currency.id === enteredCurr);
+    let newCurr = currencies.find(
+      (currency) => currency.vs_currency === convertedCurr
     );
-    return data;
+    try {
+      return reply.view("index.ejs", {
+        currencies: currencies,
+        data: data,
+        oldCurr: oldCurr,
+        newCurr: newCurr,
+      });
+    } catch (err) {
+      reply.code(500).send("Internal Server Error");
+    }
   });
 };
